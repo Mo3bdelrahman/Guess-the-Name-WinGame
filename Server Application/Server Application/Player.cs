f5fac1a49d349e047458e17684a19eca8121c32d
Abdulrahman <abdulrahman.rehann@gmail.com>
2024-02-18 14:55:58 +0200

    moved to new project

--- Server Application/Server Application/Player.cs
+++ Server Application/Server Application/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,23 @@ namespace Server_Application
 {
     internal class Player
     {
+        TcpClient client;
+        string name;
+        PlayerState state;
+        public Player(TcpClient client, string name)
+        {
+            this.client = client;
+            this.name = name;
+        }
+
+        public TcpClient getClient()
+        { return client; }
+        public string getName()
+        { return name; }
+        public PlayerState getState()
+        { return state; }
+        public void setState(PlayerState state)
+        { this.state = state; }
+
     }
 }
--- Server Application/Server Application/Room.cs
+++ Server Application/Server Application/Room.cs
@@ -8,5 +8,41 @@ namespace Server_Application
 {
     internal class Room
     {
+        Player owner;
+        Player guest;
+        Game game;
+        RoomState state;
+        bool playAgain;
+        public Room(Player owner)
+        {
+            this.owner = owner;
+            state = RoomState.Waiting;
+        }
+
+        private void CreateGame()
+        {
+            game = new Game(owner, guest);
+            state = RoomState.Running;
+        }
+        private void guestHasJoined(Player guest)
+        {
+            this.guest = guest;
+            CreateGame();
+        }
+        private void PlayAgain()
+        {
+            if (playAgain)
+            {
+                CreateGame();
+            }
+        }
+        //public bool IsRoomCompleted()
+        //{
+        //    if (game.IsGameCompleted())
+        //    {
+        //        PlayAgain();
+        //    }
+        //}
+
     }
 }
