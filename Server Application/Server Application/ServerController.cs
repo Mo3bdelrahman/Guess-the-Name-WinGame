91b7dd47a9477c32a1fd77847368798543109dba
Abdulrahman <abdulrahman.rehann@gmail.com>
2024-02-18 17:08:39 +0200

    Some changes

--- Server Application/Server Application/ServerController.cs
+++ Server Application/Server Application/ServerController.cs
@@ -15,10 +15,6 @@ namespace Server_Application
 {
     internal static class ServerController
     {
-        static List<Player> Players = new List<Player>();
-        static List<Room> Rooms = new List<Room>();
-        static TcpListener server;
-
         public static void RequestHandeller<T1>(NetworkStream networkStream ,Request request, T1 param1)
         {
             try
@@ -96,65 +92,9 @@ namespace Server_Application
             catch (Exception ex) { MessageBox.Show("From ResponseHandeller " + ex.Message); }     
         }
         
-
-        private static void ServerThread()
+        private static void BroadcastToClients(string message, List<TcpClient> clients)
         {
-            server = new TcpListener(IPAddress.Any, 8080);
-            server.Start();
-
-            MessageBox.Show("Server Started");
-
-            try
-            {
-                while (true)
-                {
-                    TcpClient client = server.AcceptTcpClient();
-
-                    Thread clientThread = new Thread(new ParameterizedThreadStart(ConnectToClient));
-                    clientThread.Start(client);
-                }
-            }
-            finally
-            {
-                server.Stop();
-            }
-        }
-
-        private static void ConnectToClient(object client)
-        {
-            TcpClient Client = (TcpClient)client;
-            // Temporary Player Until Name Is Recieved From The Login
-            Player player = new Player(Client, "Zeyad");
-
-            try
-            {
-                // Temporary Code To Keep The Connection Running
-                Players.Add(player);
-                MessageBox.Show($"{player.Name} is connected");
-                while (true)
-                { }
-
-                // ( Mohamed Abdelrahman ) -> Stream Code
-
-                // If It Is The Login Message,
-                // Initialize The Player With The Client ( From Above ) And Name ( From Login )
-                // Add The Player Using The ServerController.AddPlayer Method
-                // A Loop Must Be Added, Waiting For Messages From The Client
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("An error occurred with a client connection: " + ex.Message);
-            }
-            finally
-            {
-                player.Client.Close();
-                Players.Remove(player);
-                MessageBox.Show($"{player.Name} disconnected.");
-            }
-        }
-        private static void BroadcastToClients(string message)
-        {
-            List<TcpClient> clients = Players.Select(player => player.Client).ToList();
+            //List<TcpClient> clients = Players.Select(player => player.Client).ToList();
 
             foreach (TcpClient client in clients)
             {
@@ -171,11 +111,11 @@ namespace Server_Application
             }
         }
 
-        private static void SendToClient(string from, string to, string body, string? character = null)
+        private static void SendToClient(TcpClient client,string from, string to, string body, string? character = null)
         {
             try
             {
-                TcpClient client = Players.Find(client => client.Name == to).Client;
+                //TcpClient client = Players.Find(client => client.Name == to).Client;
 
                 // ( Mohamed Abdelrahman ) -> Stream Code
 
@@ -187,11 +127,6 @@ namespace Server_Application
             }
         }
 
-        public static void StartServer(Button startButton)
-        {
-            startButton.Enabled = false;
-            Thread serverThread = new Thread(ServerThread);
-            serverThread.Start();
-        }
+
     }
 }
